4e82abb25fff154f37036e1740b9f0ba70702006
wwtfly <wwtfly@gmail.com>
2019-05-12 15:48:25 +0800

    update

---
+++ formatDisk.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+diskname=$1
+disksize=$2
+name=$3
+partitionname=$4
+
+/usr/bin/expect <<!
+set timeout -1
+spawn fdisk $diskname
+expect "m for help"
+exec sleep 0.2
+send "n\r"
+expect "Select (default p)"
+send "p\r"
+expect "Partition number*"
+send "\r"
+expect "First sector*"
+send "\r"
+expect "Last sector*"
+send "$disksize\r"
+expect "m for help"
+exec sleep 0.5
+send "t\r"
+exec sleep 0.1
+send "\r"
+expect "Hex code (type L to list all codes)*"
+send "8e\r"
+expect "m for help"
+send "w\r"
+send "q\r"
+expect eof
+!
+
+
+pvcreate ${partitionname}
+vgcreate -s 8M ${name}_vg $partitionname
+fs=`vgdisplay ${name}_vg|grep Free*|awk '{print $5}'|sed s/[[:space:]]//g`
+lvcreate -l $fs -n ${name}_lvm ${name}_vg
+#mkfs.ext4 $diskname
