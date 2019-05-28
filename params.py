fd056a2c7196122d6387ffdff3cb07a7916545e0
wwtfly <wwtfly@gmail.com>
2019-05-29 07:31:30 +0800

    修改params

--- params.py
+++ params.py
@@ -18,19 +18,36 @@ def getParams():
 class params():
         def __init__(self,file):
                 self._file=''
-                self._loginParams=defaultdict()
-                self._partitionParams=defaultdict()
+                # 定义两个空的字典
+                self._loginParams=defaultdict(set)
+                self._partitionParams=defaultdict(list)
                 # 判断文件是否存在，如果存在则打开文件
                 if os.path.exists(file):
                         with open(file) as inf:
                                 p=yaml.load(inf,Loader=yaml.FullLoader)
                                 self._file=p
+
+                # 对yaml文件格式进行校验，如果格式由问题则退出执行
                 else:
                         print("config file :"+file+" dose not exits")
                         # print(self._file)
         # 解析参数文件，返回两个字典对象，一个是登录参数，一个是分区参数
         def getParams(self):
-                print(self._file)
+                # self.file数组中由两部分组成，一个是登录信息，一个是分区信息；
+                # self._loginParams=self._file['node']
+                # for i in range(len(self._file['node'])):
+                #         *loginParams,self._partitionParams=self._file['node'][i]
+                        # print(*loginParams)
+                #         print(self._partitionParams)
+                for params in self._file['node']:
+                        *login,disk=params.keys()
+                        # disk={disk}
+                        # print(params['ip'])
+                        # self._loginParams[params['ip']].add({key:value for key,value in params.items() if key in login})
+                        # self._partionParams = {key: value for key, value in params.items() if key == disk}
+                        self._partitionParams[params['ip']].append(params[disk])
+                        # print(self._loginParams)
+                        print(self._partitionParams)
                 return self._loginParams,self._partitionParams
 
         # 写入新的参数文件,根据用户提供的参数，主要是分区参数，生成实际参数，检查在另一个类中实现；
